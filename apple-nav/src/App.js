08aa7bb389263e22ddc67f4cb495652e29dfd8d8
krikaz <pascal.bou@protonmail.com>
2019-06-11 14:51:14 +0200

    created data

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+const data = [
+  {label: 'logo', link: '/', products: ['macbook air, macbook pro']},
+  {label: 'Mac', link: '/mac', products: ['imac', 'mac pro']},
+  {label: 'Ipad', link: '/ipad', products: ['ipad mini', 'ipad pro']},
+  {label: 'Iphone', link: '/iphone', products: ['iphone Xr', 'iphone 8']},
+  {label: 'Watch', link: '/watch', products: ['watch 4', 'watch 3']},
+  {label: 'TV', link: '/tv' products:['apple tv', 'remote']},
+  
+];
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
 
-export default App;
