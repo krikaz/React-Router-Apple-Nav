7c76ea2c729f00de86783daa6a691cd98fa78e59
krikaz <pascal.bou@protonmail.com>
2019-06-11 15:37:10 +0200

    products are displayed when clicked on parent

--- apple-nav/src/components/Nav.js
+++ apple-nav/src/components/Nav.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { Link, Route } from 'react-router-dom';
+import SubNav from './SubNav';
 
 export default function Nav(props) {
+  // console.log(props.obj);
 	return (
 		<div>
 			<Link to={props.obj.link}>{props.obj.label}</Link>
-			<Route path={props.obj.link} component={props.obj.Component} />
+      <Route 
+        exact path={props.obj.link} 
+        render={test => (
+          <SubNav {...test} props={props.obj.products} />
+        )} 
+      />
 		</div>
 	);
 }
--- apple-nav/src/components/SubNav.js
+++ apple-nav/src/components/SubNav.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
-export default function SubNav() {
-	return <div />;
+export default function SubNav(props) {
+  // console.log(props);
+	return (
+		<div>
+			{props.props.map(product => (
+				<div>{product}</div>
+			))}
+		</div>
+	);
 }
